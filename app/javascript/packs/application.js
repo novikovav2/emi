37faaf4647fa8333a024c3f1833f5fa560a5127f
Aleksandr Novikov <novikovav2@gmail.com>
2020-11-23 20:21:01 +0400

    Add toast

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+window.jQuery = $;
+window.$ = $;
+
+document.addEventListener("turbolinks:load", () => {
+    $('.toast').toast('show')
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
