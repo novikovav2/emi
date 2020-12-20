ac0f5dc2e17ae0307662c90b7546628aa02a562a
Aleksandr Novikov <novikovav2@gmail.com>
2020-12-20 19:26:28 +0400

    Add pretty_status helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
       'RJ45'
     end
   end
+
+  def pretty_status(status)
+    if status
+      "<i class='fas fa-check'></i> Да".html_safe
+    else
+      "<i class='fas fa-exclamation-triangle'></i> Нет".html_safe
+    end
+  end
 end
