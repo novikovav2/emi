65cb724ebd5e9006a43674c88aed2e92c9715b8d
Aleksandr Novikov <novikovav2@gmail.com>
2021-01-06 18:57:42 +0400

    Add human_types helper to LogicalLink#Show

--- app/helpers/logical_links_helper.rb
+++ app/helpers/logical_links_helper.rb
@@ -1,2 +1,12 @@
 module LogicalLinksHelper
+  def human_types(material)
+    case material
+      when 'patchcord'
+        'Патчкорд'
+      when 'sks'
+        'СКС'
+      when 'new_patchcord'
+        'Новый патчкорд'
+    end
+  end
 end
